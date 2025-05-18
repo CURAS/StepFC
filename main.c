3d0328c753eda62bd188e0972382b3fd54aae3bc
CURAS <3398519433@qq.com>
2025-05-18 21:51:14 +0800

    Step6完成

--- main.c
+++ main.c
@@ -1,4 +1,3 @@
-#define SFC_NO_SUBRENDER
 #define SFC_NO_SL
 
 #include <stdio.h>
@@ -49,10 +48,65 @@ void main_render(void* rgba)
 		palette_data[i] = sfc_stdpalette[g_famicom->ppu.palette_indexes[i]].data;
 	palette_data[4] = palette_data[8] = palette_data[12] = palette_data[0];
 
+	const uint8_t* name_table = g_famicom->ppu.banks[8];
+	const uint8_t* pattern_table = (!(g_famicom->ppu.registers.ctrl & SFC_PPU_FLAG_BGPATN)) ? g_famicom->ppu.banks[0] : g_famicom->ppu.banks[4];
 	for (unsigned i = 0; i <= 256 * 240; ++i)
-		data[i] = get_pixel(i & 0xff, i >> 8, 
-			g_famicom->ppu.banks[8], 
-			(!(g_famicom->ppu.registers.ctrl & SFC_PPU_FLAG_BGPATN))?g_famicom->ppu.banks[0] : g_famicom->ppu.banks[4]);
+		data[i] = get_pixel(i & 0xff, i >> 8, name_table, pattern_table);
+}
+
+int sub_render(void* rgba)
+{
+	uint32_t* data = rgba;
+
+	for (int i = 0; i < 16; ++i)
+		palette_data[i] = sfc_stdpalette[g_famicom->ppu.palette_indexes[i + 16]].data;
+	palette_data[4] = palette_data[8] = palette_data[12] = palette_data[0];
+
+	for (unsigned i = 0; i <= 256 * 240; ++i)
+		data[i] = palette_data[0];
+
+	const uint8_t* pattern_table = (!(g_famicom->ppu.registers.ctrl & SFC_PPU_FLAG_SPPATN)) ? g_famicom->ppu.banks[0] : g_famicom->ppu.banks[4];
+	for (int i = 63; i >= 0; --i)
+	{
+		uint8_t* ptr = g_famicom->ppu.sprite_ram + i * 4;
+		uint8_t xx = ptr[3];
+		uint8_t yy = ptr[0];
+		uint8_t ii = ptr[1];
+		uint8_t aa = ptr[2];
+
+		if (yy >= 0xEF)
+			continue;
+
+		const uint8_t* pattern_bit0 = pattern_table + 16 * ii;
+		const uint8_t* pattern_bit1 = pattern_bit0 + 8;
+
+		uint8_t pattern_bit32 = aa & 0x3;
+
+		const uint8_t forward[] = { 0, 1, 2, 3, 4, 5, 6, 7 };
+		const uint8_t reverse[] = { 7, 6, 5, 4, 3, 2, 1, 0 };
+		const uint8_t* x_dir = forward;
+		const uint8_t* y_dir = forward;
+		if (aa & 0x40)
+			x_dir = reverse;
+		if (aa & 0x80)
+			y_dir = reverse;
+
+		for (uint8_t i = 0; i < 8; i++)
+		{
+			for (uint8_t j = 0; j < 8; j++)
+			{
+				uint8_t offset = i;
+				uint8_t shift = 7 - j;
+				uint8_t result = (pattern_bit0[offset] & (1 << shift)) >> shift;
+				result |= (((pattern_bit1[offset] & (1 << shift)) >> shift) << 1);
+				result |= (pattern_bit32 << 2);
+
+				data[(yy + 1 + y_dir[i]) * 256 + (xx + x_dir[j])] = palette_data[result];
+			}
+		}
+	}
+
+	return 1;
 }
 
 void user_input(int index, unsigned char data)
--- sfc_addr4020.c
+++ sfc_addr4020.c
@@ -1,7 +1,33 @@
 #include <assert.h>
 #include <stdint.h>
+#include <string.h>
 #include "sfc_famicom.h"
 #include "sfc_cpu.h"
+
+extern uint8_t* sfc_get_dma_address(uint16_t address, sfc_famicom_t* famicom)
+{
+	switch (address >> 13)
+	{
+	case 0:
+		return famicom->main_memory + (address & (uint16_t)0x07FF);
+	case 1:
+		assert(!"PPU REG!");
+		return NULL;
+	case 2:
+		assert(!"TODO");
+		return NULL;
+	case 3:
+		return famicom->save_memory + (address & (uint16_t)0x1FFF);
+	case 4:
+	case 5:
+	case 6:
+	case 7:
+		return famicom->prg_banks[address >> 13] + (address & (uint16_t)0x1FFF);
+	default:
+		assert(!"invalid address");
+		return NULL;
+	}
+}
 
 extern uint8_t sfc_read_cpu_address4020(uint16_t address, sfc_famicom_t* famicom)
 {
@@ -25,6 +51,9 @@ extern void sfc_write_cpu_address4020(uint16_t address, uint8_t data, sfc_famico
 {
 	switch (address & 0x1F)
 	{
+	case 0x14:
+		memcpy(famicom->ppu.sprite_ram, (const void*)sfc_get_dma_address((uint16_t)data << 8, famicom), 256);
+		break;
 	case 0x16:
 		if (data & 0x1)
 			famicom->button_index_1 = famicom->button_index_2 = -1;
